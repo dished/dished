0cf9b466f618cbc074d269d0c8c5a3cf001d8af1
clementfung <ClementFung@ClementFung-HP.(none)>
2013-03-10 14:20:32 -0400

    Initial Project Commit

---
+++ gen/com/example/dished_mainproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.dished_mainproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/dished_mainproject/DishOptions.java
@@ -0,0 +1,29 @@
+package com.example.dished_mainproject;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DishOptions extends Activity {
+
+	TextView title;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dishoptions);
+        
+        title = (TextView) findViewById(R.id.largetext);
+        
+        Bundle extras = getIntent().getExtras();
+        
+        String fn = extras.getString("first_name");
+        
+        Toast.makeText(this, fn + " has been saved to your dishes.", Toast.LENGTH_LONG).show();
+        
+	}
+	
+}
+
