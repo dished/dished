7f3293a72184eb14b0d47329fc61b05617389d9e
clementfung <ClementFung@ClementFung-HP.(none)>
2013-03-17 12:44:59 -0400

    Introducing DbTest framework

--- Dished/src/com/example/dished/MainActivity.java
+++ Dished/src/com/example/dished/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.dished;
 
+
+import java.util.List;
+import java.util.Map;
+
 import com.example.dished.R;
 import com.example.dished.database.DbConnector;
+import com.example.dished.database.DishedTable;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 // Activity = anything that can be displayed on phone
 public class MainActivity extends Activity{
@@ -38,6 +45,32 @@ public class MainActivity extends Activity{
     public void ListView (View view){
     	Intent intent = new Intent(this, ListViewActivity.class);
     	startActivity(intent);
+    	
+    }
+    
+    public void DbTest (View view) {
+    	
+    	Toast.makeText(this, "Running a DbTest", Toast.LENGTH_LONG).show();
+    	DbConnector db = new DbConnector();
+    	ContentValues cv = new ContentValues();
+    	
+    	cv.put(DishedTable.COL_DISH, "Kung Pao Chicken");
+    	cv.put(DishedTable.COL_OVERALL, "Awesome");
+    	db.insertRecord(cv);
+    	
+    	cv.put(DishedTable.COL_DISH, "Kung Pao Chicken2");
+    	cv.put(DishedTable.COL_OVERALL, "Sucked");
+    	db.insertRecord(cv);
+    	
+    	Log.i("DbTest", "Check1");
+    	
+    	List<Map<String,Object>> reviewList = db.readAllRecords();
+    	
+    	for (Map<String,Object> review : reviewList) {
+    		Log.i("DbTest", review.get(DishedTable.COL_DISH).toString());
+    		Log.i("DbTest", review.get(DishedTable.COL_OVERALL).toString());
+    	}
+    	
     	
     }
     
--- Dished/src/com/example/dished/database/DbConnector.java
+++ Dished/src/com/example/dished/database/DbConnector.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import android.content.ContentValues;
 import android.database.Cursor;
@@ -14,7 +12,6 @@ import android.database.sqlite.SQLiteDatabase;
 public class DbConnector {
 
 	DishedDbHelper dbHelper;
-	Logger logger;
 	
 	public DbConnector() {
 		dbHelper = new DishedDbHelper(null);
@@ -23,7 +20,6 @@ public class DbConnector {
 	public long insertRecord(ContentValues values) {
 	
 		SQLiteDatabase db = dbHelper.getWritableDatabase();
-		logger.info("InfoTest");
 		long newRowId = 0;
 		newRowId = db.insert(DishedTable.TABLE_NAME, null, values);
 		return newRowId;
@@ -32,29 +28,31 @@ public class DbConnector {
 	public List<Map<String,Object>> readAllRecords() {
 		
 		SQLiteDatabase db = dbHelper.getReadableDatabase();
-		String[] columns = {DishedTable.COL_DISH,
-		DishedTable.COL_RESTAURANT,
-		DishedTable.COL_PRICE,
-		DishedTable.COL_OVERALL,
-		DishedTable.COL_HEAT,
-		DishedTable.COL_SWEET,
-		DishedTable.COL_TIME,
-		DishedTable.COL_SIZE,
-		DishedTable.COL_EXTRA};
+		String[] columns = {
+			DishedTable.COL_DISH,
+			DishedTable.COL_RESTAURANT,
+			DishedTable.COL_PRICE,
+			DishedTable.COL_OVERALL,
+			DishedTable.COL_HEAT,
+			DishedTable.COL_SWEET,
+			DishedTable.COL_TIME,
+			DishedTable.COL_SIZE,
+			DishedTable.COL_EXTRA
+		};
 		
 		Cursor c = db.query(DishedTable.TABLE_NAME, columns, null, null, null, null, DishedTable.COL_DISH + " DESC");
 		List<Map<String,Object>> reviewList = new ArrayList<Map<String,Object>>();
 		
 		if (c.moveToFirst()) {
 			String element = "";
-			while (!c.isAfterLast()) {
+			do {
 				Map<String,Object> review = new HashMap<String,Object>();
 				for (int i = 0; i < columns.length; i++) {
 					element = c.getString(c.getColumnIndexOrThrow(columns[i]));
 					review.put(columns[i], element);
 				}
 				reviewList.add(review);
-			}
+			} while (c.moveToNext());
 		}
 		
 		return reviewList;
